3d34b3acd3950ec39f4f61522bede03fa78531f0
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2023-07-01 20:19:02 +0200

    Update light, fix condition check on switch mistake.

--- custom_components/ezviz_cloud/light.py
+++ custom_components/ezviz_cloud/light.py
@@ -1,14 +1,20 @@
 """Support for EZVIZ light entity."""
 from __future__ import annotations
 
+from dataclasses import dataclass
 from typing import Any
 
-from pyezviz.constants import DeviceCatagories, DeviceSwitchType, SupportExt
+from pyezviz.constants import DeviceSwitchType, SupportExt
 from pyezviz.exceptions import HTTPError, PyEzvizError
 
-from homeassistant.components.light import ATTR_BRIGHTNESS, ColorMode, LightEntity
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS,
+    ColorMode,
+    LightEntity,
+    LightEntityDescription,
+)
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util.percentage import (
@@ -24,10 +30,32 @@ PARALLEL_UPDATES = 1
 BRIGHTNESS_RANGE = (1, 255)
 
 
+@dataclass
+class EzvizLightEntityDescriptionMixin:
+    """Mixin values for EZVIZ light entities."""
+
+    supported_ext: str
+
+
+@dataclass
+class EzvizLightEntityDescription(
+    LightEntityDescription, EzvizLightEntityDescriptionMixin
+):
+    """Describe a EZVIZ light."""
+
+
+LIGHT_TYPE = EzvizLightEntityDescription(
+    key="light",
+    name="Light",
+    translation_key="light",
+    supported_ext=str(SupportExt.SupportAlarmLight.value),
+)
+
+
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """Set up EZVIZ sensors based on a config entry."""
+    """Set up EZVIZ lights based on a config entry."""
     coordinator: EzvizDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
         DATA_COORDINATOR
     ]
@@ -36,7 +64,7 @@ async def async_setup_entry(
         EzvizLight(coordinator, camera)
         for camera in coordinator.data
         for capibility, value in coordinator.data[camera]["supportExt"].items()
-        if capibility == str(SupportExt.SupportAlarmLight.value)
+        if capibility == LIGHT_TYPE.supported_ext
         if value == "1"
     )
 
@@ -53,29 +81,17 @@ class EzvizLight(EzvizEntity, LightEntity):
         coordinator: EzvizDataUpdateCoordinator,
         serial: str,
     ) -> None:
-        """Initialize the sensor."""
+        """Initialize the light."""
         super().__init__(coordinator, serial)
-        self.battery_cam_type = bool(
-            self.data["device_category"]
-            == DeviceCatagories.BATTERY_CAMERA_DEVICE_CATEGORY.value
-        )
         self._attr_unique_id = f"{serial}_Light"
-        self._attr_name = "Light"
-
-    @property
-    def brightness(self) -> int | None:
-        """Return the brightness of this light between 0..255."""
-        return round(
+        self.entity_description = LIGHT_TYPE
+        self._attr_is_on = self.data["switches"][DeviceSwitchType.ALARM_LIGHT.value]
+        self._attr_brightness = round(
             percentage_to_ranged_value(
                 BRIGHTNESS_RANGE,
                 self.coordinator.data[self._serial]["alarm_light_luminance"],
             )
         )
-
-    @property
-    def is_on(self) -> bool:
-        """Return the state of the light."""
-        return self.data["switches"][DeviceSwitchType.ALARM_LIGHT.value]
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn on light."""
@@ -104,7 +120,10 @@ class EzvizLight(EzvizEntity, LightEntity):
             ) from err
 
         if update_ok:
-            await self.coordinator.async_request_refresh()
+            if ATTR_BRIGHTNESS in kwargs:
+                self._attr_brightness = kwargs[ATTR_BRIGHTNESS]
+            self._attr_is_on = True
+            self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off light."""
@@ -122,4 +141,22 @@ class EzvizLight(EzvizEntity, LightEntity):
             ) from err
 
         if update_ok:
-            await self.coordinator.async_request_refresh()
+            if ATTR_BRIGHTNESS in kwargs:
+                self._attr_brightness = kwargs[ATTR_BRIGHTNESS]
+            self._attr_is_on = False
+            self.async_write_ha_state()
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        if self.data["switches"].get(DeviceSwitchType.ALARM_LIGHT.value) is None:
+            return
+
+        self._attr_brightness = round(
+            percentage_to_ranged_value(
+                BRIGHTNESS_RANGE,
+                self.data["alarm_light_luminance"],
+            )
+        )
+        self._attr_is_on = self.data["switches"][DeviceSwitchType.ALARM_LIGHT.value]
+        super()._handle_coordinator_update()
