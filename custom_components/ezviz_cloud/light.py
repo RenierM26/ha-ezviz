4fd60fa3f0961e4a6878d1cb231badde1597ba94
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2023-07-16 07:34:53 +0200

    Light cleanup.

--- custom_components/ezviz_cloud/light.py
+++ custom_components/ezviz_cloud/light.py
@@ -46,7 +46,6 @@ class EzvizLightEntityDescription(
 
 LIGHT_TYPE = EzvizLightEntityDescription(
     key="light",
-    name="Light",
     translation_key="light",
     supported_ext=str(SupportExt.SupportAlarmLight.value),
 )
@@ -61,7 +60,7 @@ async def async_setup_entry(
     ]
 
     async_add_entities(
-        EzvizLight(coordinator, camera)
+        EzvizLight(coordinator, camera, LIGHT_TYPE)
         for camera in coordinator.data
         for capibility, value in coordinator.data[camera]["supportExt"].items()
         if capibility == LIGHT_TYPE.supported_ext
@@ -72,6 +71,7 @@ async def async_setup_entry(
 class EzvizLight(EzvizEntity, LightEntity):
     """Representation of a EZVIZ light."""
 
+    entity_description: EzvizLightEntityDescription
     _attr_has_entity_name = True
     _attr_color_mode = ColorMode.BRIGHTNESS
     _attr_supported_color_modes = {ColorMode.BRIGHTNESS}
@@ -80,11 +80,12 @@ class EzvizLight(EzvizEntity, LightEntity):
         self,
         coordinator: EzvizDataUpdateCoordinator,
         serial: str,
+        entity_description: EzvizLightEntityDescription,
     ) -> None:
         """Initialize the light."""
         super().__init__(coordinator, serial)
         self._attr_unique_id = f"{serial}_Light"
-        self.entity_description = LIGHT_TYPE
+        self.entity_description = entity_description
         self._attr_is_on = self.data["switches"][DeviceSwitchType.ALARM_LIGHT.value]
         self._attr_brightness = round(
             percentage_to_ranged_value(
@@ -92,11 +93,6 @@ class EzvizLight(EzvizEntity, LightEntity):
                 self.coordinator.data[self._serial]["alarm_light_luminance"],
             )
         )
-
-    @property
-    def is_on(self) -> bool:
-        """Return true if light is on."""
-        return bool(self._attr_is_on)
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn on light."""
@@ -106,56 +102,44 @@ class EzvizLight(EzvizEntity, LightEntity):
                     BRIGHTNESS_RANGE, kwargs[ATTR_BRIGHTNESS]
                 )
 
-                update_ok = await self.hass.async_add_executor_job(
+                if await self.hass.async_add_executor_job(
                     self.coordinator.ezviz_client.set_floodlight_brightness,
                     self._serial,
                     data,
-                )
-            else:
-                update_ok = await self.hass.async_add_executor_job(
-                    self.coordinator.ezviz_client.switch_status,
-                    self._serial,
-                    DeviceSwitchType.ALARM_LIGHT.value,
-                    1,
-                )
+                ):
+                    self._attr_brightness = kwargs[ATTR_BRIGHTNESS]
+
+            if await self.hass.async_add_executor_job(
+                self.coordinator.ezviz_client.switch_status,
+                self._serial,
+                DeviceSwitchType.ALARM_LIGHT.value,
+                1,
+            ):
+                self._attr_is_on = True
+                self.async_write_ha_state()
 
         except (HTTPError, PyEzvizError) as err:
-            raise HomeAssistantError(
-                f"Failed to turn on light {self._attr_name}"
-            ) from err
-
-        if update_ok:
-            if ATTR_BRIGHTNESS in kwargs:
-                self._attr_brightness = kwargs[ATTR_BRIGHTNESS]
-            self._attr_is_on = True
-            self.async_write_ha_state()
+            raise HomeAssistantError(f"Failed to turn on light {self.name}") from err
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off light."""
         try:
-            update_ok = await self.hass.async_add_executor_job(
+            if await self.hass.async_add_executor_job(
                 self.coordinator.ezviz_client.switch_status,
                 self._serial,
                 DeviceSwitchType.ALARM_LIGHT.value,
                 0,
-            )
+            ):
+                self._attr_is_on = False
+                self.async_write_ha_state()
 
         except (HTTPError, PyEzvizError) as err:
-            raise HomeAssistantError(
-                f"Failed to turn off light {self._attr_name}"
-            ) from err
-
-        if update_ok:
-            if ATTR_BRIGHTNESS in kwargs:
-                self._attr_brightness = kwargs[ATTR_BRIGHTNESS]
-            self._attr_is_on = False
-            self.async_write_ha_state()
+            raise HomeAssistantError(f"Failed to turn off light {self.name}") from err
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-        if self.data["switches"].get(DeviceSwitchType.ALARM_LIGHT.value):
-            self._attr_is_on = self.data["switches"][DeviceSwitchType.ALARM_LIGHT.value]
+        self._attr_is_on = self.data["switches"].get(DeviceSwitchType.ALARM_LIGHT.value)
 
         if isinstance(self.data["alarm_light_luminance"], int):
             self._attr_brightness = round(
