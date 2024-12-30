6573fb82a41ead3b58a21cbff037796412fc8739
Renier Moorcroft <66512715+RenierM26@users.noreply.github.com>
2024-12-30 16:16:47 +0200

    Fix password key error when ignored. (#152)
    Fix password key error when ignored.

--- custom_components/ezviz_cloud/image.py
+++ custom_components/ezviz_cloud/image.py
@@ -57,7 +57,7 @@ class EzvizLastMotion(EzvizEntity, ImageEntity):
         )
         camera = hass.config_entries.async_entry_for_domain_unique_id(DOMAIN, serial)
         self.alarm_image_password = (
-            camera.data[CONF_PASSWORD] if camera is not None else None
+            camera.data.get(CONF_PASSWORD) if camera is not None else None
         )
 
     async def _async_load_image_from_url(self, url: str) -> Image | None:
